4430bcc3306a99121a612c6f6fca6a827b50c760
chiknwy <chikogitasatria23@gmail.com>
2023-10-09 20:15:00 +0800

    push

--- script.js
+++ script.js
@@ -1,41 +1,67 @@
-function calculateFibonacci() {
-    const n = parseInt(document.getElementById("fibonacci-input").value);
-    if (!isNaN(n) && n >= 0) {
-        const result = fibonacci(n);
-        document.getElementById("fibonacci-result").textContent = result;
-    } else {
-        alert("Masukkan angka positif.");
+document.addEventListener('DOMContentLoaded', function () {
+
+    function handleIntersection(entries, observer) {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.remove('opacity-0', 'translate-y-8');
+                entry.target.classList.add('opacity-100', 'translate-y-0');
+            }
+        });
     }
+
+    const sectionObserver = new IntersectionObserver(handleIntersection, {
+        root: null, 
+        rootMargin: '0px', 
+        threshold: 0.1, 
+    });
+
+    const sections = document.querySelectorAll('.transition-opacity.transform');
+    sections.forEach((section) => {
+        sectionObserver.observe(section);
+    });
+
+    var scrollLinks = document.querySelectorAll('a[href^="#"]');
+for (var i = 0; i < scrollLinks.length; i++) {
+    scrollLinks[i].addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        for (var j = 0; j < scrollLinks.length; j++) {
+            scrollLinks[j].classList.remove('selected');
+        }
+        
+        this.classList.add('selected');
+        
+        var targetId = this.getAttribute('href').substring(1);
+        var targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 100,
+                behavior: 'smooth'
+            });
+        }
+    });
 }
 
-function fibonacci(n) {
-    if (n <= 1) return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
-}
 
-function calculateVolume() {
-    const selectedShape = document.getElementById("shape-select").value;
-    const kubusSide = parseFloat(document.getElementById("kubus-side").value);
-    const bolaRadius = parseFloat(document.getElementById("bola-radius").value);
+    const menuToggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
 
-    if (selectedShape === "kubus" && !isNaN(kubusSide) && kubusSide >= 0) {
-        const volume = kubusSide ** 3;
-        document.getElementById("volume-result").textContent = `Volume Kubus: ${volume.toFixed(2)}`;
-    } else if (selectedShape === "bola" && !isNaN(bolaRadius) && bolaRadius >= 0) {
-        const volume = (4 / 3) * Math.PI * (bolaRadius ** 3);
-        document.getElementById("volume-result").textContent = `Volume Bola: ${volume.toFixed(2)}`;
-    } else {
-        alert("Masukkan angka positif.");
-    }
-}
+    menuToggle.addEventListener("click", function () {
+        menu.classList.toggle("hidden");
+        menuToggle.classList.toggle("rotate-90");
 
-document.getElementById("shape-select").addEventListener("change", function () {
-    const selectedShape = this.value;
-    if (selectedShape === "kubus") {
-        document.getElementById("kubus-form").style.display = "block";
-        document.getElementById("bola-form").style.display = "none";
-    } else if (selectedShape === "bola") {
-        document.getElementById("kubus-form").style.display = "none";
-        document.getElementById("bola-form").style.display = "block";
-    }
+        if (menu.classList.contains("hidden")) {
+            menuToggle.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                </svg>
+            `;
+        } else {
+            menuToggle.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                </svg>
+            `;
+        }
+    });
 });
